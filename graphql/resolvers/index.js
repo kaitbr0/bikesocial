296558f5bdf95157ef9f7b6770711cb25da11baf
kaitbr0 <humansare@gmail.com>
2021-04-03 21:54:35 -0400

    FEATURE: get/create post

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -10,5 +10,6 @@ module.exports = {
   },
   Mutation: {
     ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
   },
 };
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 module.exports = {
   Query: {
     async getPosts() {
@@ -8,6 +9,31 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+      const post = await newPost.save();
+      return post;
     },
   },
 };
