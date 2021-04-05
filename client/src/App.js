d7d78c3e56d4aebc1ec27ed90b9c98a2739ca2cf
kaitbr0 <humansare@gmail.com>
2021-04-04 21:39:36 -0400

    FEATURE: navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello World</header>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
